0a79e8d850f32cde96d456ebb73de79c8454ce7f
Maruthi Kumar <maruthi.venkata@columbia.com>
2020-07-03 22:24:17 +0530

    broken our homepage into smaller components and finished sysling menuitem

---
+++ src/components/directory/Directory.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import MenuItem from '../menu-item/MenuItem';
+
+import './directory.scss';
+
+class Directory extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections : [
+                {
+                  title: 'hats',
+                  imageUrl: 'hats.png',
+                  id: 1,
+                  linkUrl: 'shop/hats'
+                },
+                {
+                  title: 'jackets',
+                  imageUrl: 'jackets.png',
+                  id: 2,
+                  linkUrl: 'shop/jackets'
+                },
+                {
+                  title: 'sneakers',
+                  imageUrl: 'sneakers.png',
+                  id: 3,
+                  linkUrl: 'shop/sneakers'
+                },
+                {
+                  title: 'womens',
+                  imageUrl: 'womens.png',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: 'shop/womens'
+                },
+                {
+                  title: 'mens',
+                  imageUrl: 'men.png',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
